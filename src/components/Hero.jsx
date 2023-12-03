ad5299d02019d2a0826c0b6682ba7e10a5fec694
OmarTl2005 <hiba41145@gmail.com>
2023-12-03 19:46:53 +0100

    fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,8 +9,8 @@ const Hero = () => {
         <main className="w-full xsm:w-full md:w-[80%] xsm:pt-[150px] md:pt-0 self-center h-full flex flex-row items-center justify-center" >
             <section className="flex w-[4%] items-center flex-col justify-center md:mr-[30px] xsm:pl-4 md:pl-0 sm:pl-6 z-[1000000]">
                 <a alt="instagram account" href="https://www.instagram.com/estn_sports_club" target="_blank" rel="noreferrer"><SiInstagram className="md:text-4xl sm:text-3xl xsm:text-2xl text-black mb-[15px] hover:text-[#cd486b] transition-all duration-500 hover:-translate-y-2 cursor-pointer" /></a>
-                <a alt="not yet" href="not yet"><FaFacebook  className="md:text-4xl xsm:text-2xl sm:text-3xl text-black mb-[15px] hover:text-[#3b5998] transition-all duration-500 hover:-translate-y-2 cursor-pointer"/></a>
-                <a alt="not yet" href="not yet"><BiLogoGmail className="md:text-4xl xsm:text-2xl sm:text-3xl text-black mb-[15px] hover:text-[#c71610] transition-all duration-500 hover:-translate-y-2 cursor-pointer" /></a>
+                <a alt="not yet" href="/"><FaFacebook  className="md:text-4xl xsm:text-2xl sm:text-3xl text-black mb-[15px] hover:text-[#3b5998] transition-all duration-500 hover:-translate-y-2 cursor-pointer"/></a>
+                <a alt="not yet" href="/"><BiLogoGmail className="md:text-4xl xsm:text-2xl sm:text-3xl text-black mb-[15px] hover:text-[#c71610] transition-all duration-500 hover:-translate-y-2 cursor-pointer" /></a>
             </section>
             <section className="xsm:flex-col-reverse md:flex-row flex-col-reverse w-full self-end h-full flex items-center justify-between" >
                 <section className="md:w-[70%] xsm:w-[90%] flex flex-col justify-center items-center">
