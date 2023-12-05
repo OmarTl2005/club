30b8b0f0d1c4a9a6dc67940a0e471b48eead0ccd
OmarTl2005 <hiba41145@gmail.com>
2023-12-05 20:22:11 +0100

    added the socials

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,9 +8,9 @@ const Home = () => {
     return (
         <main className="w-full xsm:w-full md:w-[80%] xsm:pt-[100px] md:pt-0 self-center xsm:h-full md:h-[83vh] flex flex-row items-center justify-center" >
             <section className="flex w-[4%] items-center flex-col justify-center md:mr-[30px] xsm:pl-4 md:pl-0 sm:pl-6 z-[1000000]">
-                <a alt="instagram account" href="https://www.instagram.com/estn_sports_club" target="_blank" rel="noreferrer"><SiInstagram className="md:text-4xl sm:text-3xl xsm:text-2xl text-black mb-[15px] hover:text-[#cd486b] transition-all duration-500 hover:-translate-y-2 cursor-pointer" /></a>
-                <a alt="not yet" href="/"><FaFacebook  className="md:text-4xl xsm:text-2xl sm:text-3xl text-black mb-[15px] hover:text-[#3b5998] transition-all duration-500 hover:-translate-y-2 cursor-pointer"/></a>
-                <a alt="not yet" href="/"><BiLogoGmail className="md:text-4xl xsm:text-2xl sm:text-3xl text-black mb-[15px] hover:text-[#c71610] transition-all duration-500 hover:-translate-y-2 cursor-pointer" /></a>
+                <a alt="instagram" href="https://www.instagram.com/estn_sports_club" target="_blank" rel="noreferrer"><SiInstagram className="md:text-4xl sm:text-3xl xsm:text-2xl text-black mb-[15px] hover:text-[#cd486b] transition-all duration-500 hover:-translate-y-2 cursor-pointer" /></a>
+                <a alt="Facebook" href="https://www.facebook.com/profile.php?id=61553553870800&mibextid=ZbWKwL" target="_blank" rel="noreferrer"><FaFacebook  className="md:text-4xl xsm:text-2xl sm:text-3xl text-black mb-[15px] hover:text-[#3b5998] transition-all duration-500 hover:-translate-y-2 cursor-pointer"/></a>
+                <a alt="Send an email" href="https://mail.google.com/mail/?view=cm&fs=1&to=estsportsclub@gmail.com" target="_blank" rel="noreferrer"><BiLogoGmail className="md:text-4xl xsm:text-2xl sm:text-3xl text-black mb-[15px] hover:text-[#c71610] transition-all duration-500 hover:-translate-y-2 cursor-pointer" /></a>
             </section>
             <section className="xsm:flex-col-reverse md:flex-row flex-col-reverse w-full self-end h-full flex items-center justify-between" >
                 <section className="md:w-[70%] xsm:w-[90%] flex flex-col justify-center items-center">
