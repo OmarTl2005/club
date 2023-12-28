177153a4c288f50ebbea30e09f5ff5dbf565188a
OmarTl2005 <hiba41145@gamil.com>
2023-12-29 00:10:07 +0100

    made it way more prettier

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -17,18 +17,18 @@ const Activities = () => {
         <div className="flex items-center justify-center flex-col self-center xsm:w-full md:w-[80%] h-full">
             <h1 className="font-outfit border-b-2 mb-11 border-b-black text-green-700 md:text-4xl xsm:text-3xl text-bold hover:-translate-y-2 cursor-pointer transition-all duration-500">Activities</h1>
             <div className='md:w-[80%] xsm:w-full flex xsm:flex-wrap md:flex-nowrap items-center justify-around font-outfit my-6 gap-1'>
-                <button className='isolate aspect-video bg-slate-900/90 shadow-2xl ring-1 ring-black/7 transition-all duration-500 hover:-translate-y-1 text-white hover:bg-transparent hover:text-black rounded-full py-2 px-4 ' onClick={() => resetFilter()}>All</button>
-                <button className='isolate aspect-video bg-slate-900/90 shadow-2xl ring-1 ring-black/7 transition-all duration-500 hover:-translate-y-1 text-white hover:bg-transparent hover:text-black rounded-full py-2 px-4 ' onClick={() => filterItems("football")}>Football</button>
-                <button className='isolate aspect-video bg-slate-900/90 shadow-2xl ring-1 ring-black/7 transition-all duration-500 hover:-translate-y-1 text-white hover:bg-transparent hover:text-black rounded-full py-2 px-4 ' onClick={() => filterItems("basketball")}>Basketball</button>
-                <button className='isolate aspect-video bg-slate-900/90 shadow-2xl ring-1 ring-black/7 transition-all duration-500 hover:-translate-y-1 text-white hover:bg-transparent hover:text-black rounded-full py-2 px-4 ' onClick={() => filterItems("volleyball")}>Volleyball</button>
-                <button className='isolate aspect-video bg-slate-900/90 shadow-2xl ring-1 ring-black/7 transition-all duration-500 hover:-translate-y-1 text-white hover:bg-transparent hover:text-black rounded-full py-2 px-4 ' onClick={() => filterItems("handball")}>Handball</button>
-                <button className='isolate aspect-video bg-slate-900/90 shadow-2xl ring-1 ring-black/7 transition-all duration-500 hover:-translate-y-1 text-white hover:bg-transparent hover:text-black rounded-full py-2 px-4 ' onClick={() => filterItems("other")}>Other</button>
+                <button className='isolate aspect-video bg-blue-900/70 shadow-2xl ring-1 ring-black/7 transition-all duration-500 hover:-translate-y-1 text-white hover:bg-transparent hover:text-black rounded-full py-2 px-4 ' onClick={() => resetFilter()}>All</button>
+                <button className='isolate aspect-video bg-blue-900/70 shadow-2xl ring-1 ring-black/7 transition-all duration-500 hover:-translate-y-1 text-white hover:bg-transparent hover:text-black rounded-full py-2 px-4 ' onClick={() => filterItems("football")}>Football</button>
+                <button className='isolate aspect-video bg-blue-900/70 shadow-2xl ring-1 ring-black/7 transition-all duration-500 hover:-translate-y-1 text-white hover:bg-transparent hover:text-black rounded-full py-2 px-4 ' onClick={() => filterItems("basketball")}>Basketball</button>
+                <button className='isolate aspect-video bg-blue-900/70 shadow-2xl ring-1 ring-black/7 transition-all duration-500 hover:-translate-y-1 text-white hover:bg-transparent hover:text-black rounded-full py-2 px-4 ' onClick={() => filterItems("volleyball")}>Volleyball</button>
+                <button className='isolate aspect-video bg-blue-900/70 shadow-2xl ring-1 ring-black/7 transition-all duration-500 hover:-translate-y-1 text-white hover:bg-transparent hover:text-black rounded-full py-2 px-4 ' onClick={() => filterItems("handball")}>Handball</button>
+                <button className='isolate aspect-video bg-blue-900/70 shadow-2xl ring-1 ring-black/7 transition-all duration-500 hover:-translate-y-1 text-white hover:bg-transparent hover:text-black rounded-full py-2 px-4 ' onClick={() => filterItems("other")}>Other</button>
             </div>
             <div className='flex flex-wrap justify-center w-full h-full mb-4 gap-7'>
                 {filteredPosts.map((val) => (
-                    <div className="md:w-1/2 lg:w-1/3 xsm:w-[80%] md:h-[300px] hover:-translate-y-3 cursor-pointer trasition-all duration-500 flex flex-col text-center items-center justify-between isolate aspect-video rounded-3xl bg-green-700/40 shadow-2xl ring-1 ring-black/5 py-4" key={val.id}>
+                    <div className="md:w-1/2 lg:w-1/3 xsm:w-[80%] md:h-[300px] hover:-translate-y-2 cursor-pointer trasition-all duration-700 flex flex-col text-center items-center justify-between isolate aspect-video rounded-3xl bg-blue-900/50 shadow-2xl ring-1 ring-black/5 py-4" key={val.id}>
                         <img className='rounded-3xl w-[70%] h-[70%] cursor-pointer' src={ val.img } alt="football tournament" />
-                        <h1 className='text-xl font-libre text-green-700 font-semibold cursor-pointer hover:text-blue-600 trasition_all duration-700' >{val.title}:</h1>
+                        <h1 className='text-xl font-libre font-semibold cursor-pointer text-blue-600/80 trasition_all duration-700' >{val.title}:</h1>
                         <p className='text-lg font-space w-[90%]'>{val.desc}</p>
                     </div>
                 ))}
